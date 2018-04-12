045c6a23f971a2666bc99658c0f76c769c34c845
ilkiz.kiziltas <ilkiz.kiziltas@verapi.com>
2018-04-12 19:11:15 +0300

    [*] login validation controls

--- dist/js/auth.js
+++ dist/js/auth.js
@@ -1,1 +1,21 @@
 console.log("JS: auth.js");
+define(['Vue', 'axios', 'vee-validate'], function (Vue, axios, VeeValidate) {
+	Vue.use(VeeValidate)
+	new Vue({
+		el: '#portal',
+		methods: {
+			validateBeforeSubmit() {
+				this.$validator.validateAll().then((result) => {
+					if (result) {
+						// alert('Form Submitted!');
+						return;
+					}
+					// alert('Correct them errors!');
+				});
+			}
+		},
+		created() {
+			console.log("this: ", this);
+		}
+	});
+});
