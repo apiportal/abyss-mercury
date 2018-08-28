8cbff0976440225f70af5796b3df8704a2c44c2c
faik.saglar <faik.saglar@verapi.com>
2018-08-28 11:38:49 +0300

    ABYSSP-302 Subject Group API Url changed.

--- dist/js/config.js
+++ dist/js/config.js
@@ -48,7 +48,7 @@ define(['global'], function(abyss){
 		subject_app_subject_list: '/subject-apps/subject/', //uuid ABYSSP-183 GET
 		subjects_changepassword: '/subjects/{uuid}/changepassword/', //2DO ABYSSP-76 PUT oldpassword,newpassword,confirmpassword,crudsubjectid
 		// https://dev2.apiportal.com/abyss/openapi/SubjectGroup.yaml
-		user_group_list: '/subject-groups',
+		user_group_list: '/subjects/groups/', // must /
 		// https://dev2.apiportal.com/abyss/openapi/SubjectPermission.yaml
 		permission_list: '/subject-permissions', //uuid
 		permission_my_apis: '/subject-permissions/my-apis-subscriptions/subject/', //useruuid ABYSSP-182 GET
